63a35b869af8f4fe383054485466db5e04e99cac
dwilczek80 <dwilczek80@gmail.com>
2026-02-09 19:53:58 +0100

    fix: loading dnd cog in ludus

--- Ludus-Bot/cogs/dnd/__init__.py
+++ Ludus-Bot/cogs/dnd/__init__.py
@@ -6,3 +6,8 @@ Modular dimension system for interdimensional narrative RPG.
 from .dnd import InfinityAdventure
 
 __all__ = ['InfinityAdventure']
+
+
+async def setup(bot):
+    """Required setup function for Discord.py cog loading"""
+    await bot.add_cog(InfinityAdventure(bot))
